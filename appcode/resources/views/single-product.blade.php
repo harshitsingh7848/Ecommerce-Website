28b60f6a17be2b3f0ed5b2d06672f72a10bf1b9d
Harshit Singh <singhharshit@gmail.COM>
2018-03-30 10:52:36 +0530

    changes done to cart page

--- appcode/resources/views/single-product.blade.php
+++ appcode/resources/views/single-product.blade.php
@@ -282,49 +282,72 @@
       
     <script>
       $(document).ready(function(){
+        var product=new Array();
         $('#cart').click(function(){
-          /* var quantity=1;
-          var product=new Array();
-        var productId =$('#productwId').val();
-        var id ="p"+productId;
-        product["sjdjd"]=quantity;
-        alert(product.length);
- */
-        var data = {'A': 9};
+           var quantity=1;
+           var check=0;
+           var count=0;
+           var productId =$('#productwId').val();
+          //var id ="p"+productId;
+            if(localStorage.getItem('product')){   
+            var productInfo = localStorage.getItem('product');
+            var productsDetail = JSON.parse(productInfo);
+            alert(productsDetail.length);
+              
+          //localStorage.removeItem('product');  
+             for(var i=0;i<productsDetail.length;i=i+2){
+               
+              if(productsDetail[i]==productId){
+                productsDetail[i+1]=productsDetail[i+1]+quantity;
+                check=1;
+              }
+            }
+            if(check==0){
+              productsDetail.push(productId);
+              productsDetail.push(quantity);
+              count=count+quantity; 
+            }
+            localStorage.setItem('product',JSON.stringify(productsDetail)); 
+            for(var i=0;i<productsDetail.length;i++){
+            console.log(productsDetail[i]);
+          }  
+        } 
+          else{
+            product.push(productId);
+            product.push(quantity);
+            localStorage.setItem('product',JSON.stringify(product));      
+          } 
+          if(localStorage.getItem('product')){   
+            var productInfo = localStorage.getItem('product');
+            var productsDetail = JSON.parse(productInfo);
+            //alert(productsDetail.length);
+              
+          //localStorage.removeItem('product');  
+             for(var i=1;i<productsDetail.length;i=i+2){
+               count=count+productsDetail[i];
+             }
+             
+          }
+          $('.product-count').val(count);  
+        });
+      });
 
-localStorage['screenshots'] = JSON.stringify(data);
+        //var data = {'A': 9};
+/* 
+localStorage['screenshots'] = JSON.stringify(product);
 
 // Later/elsewhere:
 if(localStorage['screenshots']){
 var data = JSON.parse(localStorage['screenshots']);
 
 // 9
-console.log(data.A);
+console.log(product);
 }
-
-        
-        
+ */
+              
         /* for (var prop in product) {
           console.log( prop + ': ' + product[prop]);
         } */
-        
-          /* if(localStorage.getItem('products')){   
-            var productInfo = localStorage.getItem('products');
-            var productsDetail = JSON.parse(productInfo);
-            alert(JSON.stringify(product));
-            for (var prop in productsDetail) {
-            console.log( prop + ': ' + productsDetail[prop]);
-        }
-              
-            }
-            else{
-              
-            localStorage.setItem('products',JSON.stringify(product));
-            
-          }   */
-                  
-        });
-      });
     </script>   
 
     @endsection
